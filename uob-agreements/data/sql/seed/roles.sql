55c94d2968926ebd62bbfc7919149efd80463956
KAlalwani <khadijaalalwani79@gmail.com>
2026-07-14 16:02:54 +0300

    add automations, and permissions

---
+++ uob-agreements/data/sql/seed/roles.sql
@@ -0,0 +1,34 @@
+INSERT INTO roles
+(role_name, description)
+
+VALUES
+
+
+(
+'Agreement Creator',
+'Can create and submit agreements'
+),
+
+
+(
+'Agreement Approver',
+'Can approve or reject agreements'
+),
+
+
+(
+'Initiative Creator',
+'Can create initiatives'
+),
+
+
+(
+'Initiative Approver',
+'Can approve or reject initiatives'
+),
+
+
+(
+'System Administrator',
+'Full system access'
+);
---
+++ uob-agreements/data/sql/seed/workflows.sql
@@ -0,0 +1,20 @@
+INSERT INTO workflow_templates
+
+(name, process_type)
+
+VALUES
+
+(
+'Agreement Approval',
+'AGREEMENT'
+);
+INSERT INTO workflow_templates
+
+(name, process_type)
+
+VALUES
+
+(
+'Initiative Approval',
+'INITIATIVE'
+);
