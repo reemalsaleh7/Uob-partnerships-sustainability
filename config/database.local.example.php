2c3847cb5260b39c0444182111a2b24cea6ad0de
Fatima Ebrahim Ali Abdulla <83508576+fa1819@users.noreply.github.com>
2026-07-26 16:10:56 +0300

    h

---
+++ config/database.local.example.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'host' => '127.0.0.1',
+    'port' => '5432',
+    'dbname' => 'UOB_Partnership_and_Initiative',
+    'user' => 'postgres',
+    'password' => 'MySecurePassword123',
+];
+
