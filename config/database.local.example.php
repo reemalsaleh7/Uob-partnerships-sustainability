9ab3e7a0d1afb2a1af54e8cd2d5eb62ad48d8009
REEM SALEH <reemalmuresi200@gmail.com>
2026-07-27 15:45:12 +0300

    update database

--- config/database.local.example.php
+++ config/database.local.example.php
@@ -7,5 +7,5 @@ return [
     'port' => '5432',
     'dbname' => 'UOB_Partnership_and_Initiative',
     'user' => 'postgres',
-    'password' => 'MySecurePassword123',
+    'password' => 'CHANGE_ME',
 ];
